c5a80f10f635bcf6da8fa4b6cdad6eb75b0234ce
AGRAWALrishabh11 <rishabh.agrawal1102@gmail.com>
2025-04-09 11:09:45 +0530

    hii

---
+++ frontend/src/components/Delete.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import axios from 'axios'
+import '../App.css'
+
+const Delete = () => {
+  const handleDelete = async (e) => {
+    e.preventDefault()
+    const id = e.target.id.value;
+    await axios.delete(`http://localhost:9000/users/${id}`)
+    alert('Product deleted successfully')
+  }
+
+  return (
+    <div className="container">
+      <h1>Delete Product</h1>
+      <form onSubmit={handleDelete}>
+        <label>Product ID:</label>
+        <input type="text" placeholder="Enter Product ID" name="id" />
+        <button type="submit">Delete</button>
+      </form>
+    </div>
+  )
+}
+
+export default Delete
